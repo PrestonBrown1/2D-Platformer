a877f667142c581086339a235d4928de32b2749a
PrestonBrown1 <browpr@iu.edu>
2023-11-02 22:22:05 -0400

    Adding SpaceShip and Tweaking Physics

--- Player.gd
+++ Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -36,3 +36,4 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	floor_max_angle = deg_to_rad(60)
